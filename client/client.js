f410a41efa2acf14fa47b834333914fd7088ce58
Mark Rothermal <rothermal2@comcast.net>
2016-03-04 20:03:12 -0600

    cleaned up client js

--- client/client.js
+++ client/client.js
@@ -10,9 +10,6 @@ var marvelApiImage = {};
 var marvelApiDescription = '';
 var marvelName ='';
 var results = {};
-var apiKey ="";
-var apiTs = "";
-var apiHash ="";
 
 $(document).ready(function(){
     init();
@@ -47,18 +44,6 @@ function Employee(firstname, lastname, id , title, salary ) {
     employeeArray.push(this);
 }
 
-//var deadpool = new Employee('Wade', 'Wilson', 1999, 'Deadpool',55000);
-//var ghostRider = new Employee('Johnny','Blaze',1968,"Ghost Rider",24000);
-//var doctorStrange = new Employee('Vincent','Strange',1969,"Doctor Strange",360000);
-//var spidey = new Employee('Peter','Parker',1967,"Spider-Man",29000);
-//var magneto = new Employee('Max','Eisenhardt',1965,"Magneto",240000);
-//var cyclops = new Employee('Scott','Summers',1963,"Cyclops",47000);
-//var profX = new Employee('Charles','Xavier',1964,"Professor X",65000);
-//var beast = new Employee('Hank','Mccoy',1966,"Beast",24000);
-//var starLord = new Employee('Peter','Quill',1976,"Star-Lord",24000);
-//var hulk = new Employee('Bruce','Banner',1977,"Hulk",214000);
-//var ironMan = new Employee('Tony','Stark',1976,"Iron Man",12124000);
-//var bob = new Employee('Bob','Agent of Hydra',1986,"Bob, Agent of Hydra",55000);
 
 function deleteEmployee(){
     $(this).parent().remove();
@@ -79,84 +64,53 @@ function processForm(event){
 }
 
 
-console.log(employeeArray);
+//console.log(employeeArray);
+
+///////////
+// need to build a back end version of this.
+///////////
+//function marvel (employee){
+//    var name = employee.employeeTitle;
+//    $.ajax({
+//        type:'GET',
+//        url: "http://gateway.marvel.com//v1/public/characters?name="+name+"&",
+//        data:
+//        {"apikey":apiKey,
+//            "ts": apiTs,
+//            "hash": apiHash},
+//        headers: {   Accept:'*/*' },
+//        success: function(response){
+//            console.log(response);
+//            results = response.data.results;
+//            if(results.length > 0) {
+//                randomNum = randomNumber(0,results.length -1);
+//                marvelApiImage = results[randomNum].thumbnail.path + "/standard_xlarge.jpg";
+//                marvelCharId = results[randomNum].id;
+//                if(results[randomNum].description.length > 0) {
+//                    marvelApiDescription = results[randomNum].description;
+//                } else {
+//                    marvelApiDescription = "No Back Story Available ";
+//                }
+//
+//            } else {
+//                marvelApiImage = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
+//                marvelApiDescription = "No Back Story Available ";
+//            }
+//            employee.image = marvelApiImage;
+//            employee.description = marvelApiDescription;
+//            employee.employeeId = marvelCharId;
+//            marvelArray.push(response);
+//            AppendDom(employee);
+//        }
+//    });
+//
+//
+//}
 
 
-function marvel (employee){
-    var name = employee.employeeTitle;
-    $.ajax({
-        type:'GET',
-        url: "http://gateway.marvel.com//v1/public/characters?name="+name+"&",
-        data:
-        {"apikey":apiKey,
-            "ts": apiTs,
-            "hash": apiHash},
-        headers: {   Accept:'*/*' },
-        success: function(response){
-            console.log(response);
-            results = response.data.results;
-            if(results.length > 0) {
-                randomNum = randomNumber(0,results.length -1);
-                marvelApiImage = results[randomNum].thumbnail.path + "/standard_xlarge.jpg";
-                marvelCharId = results[randomNum].id;
-                if(results[randomNum].description.length > 0) {
-                    marvelApiDescription = results[randomNum].description;
-                } else {
-                    marvelApiDescription = "No Back Story Available ";
-                }
-
-            } else {
-                marvelApiImage = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
-                marvelApiDescription = "No Back Story Available ";
-            }
-            employee.image = marvelApiImage;
-            employee.description = marvelApiDescription;
-            employee.employeeId = marvelCharId;
-            marvelArray.push(response);
-            AppendDom(employee);
-        }
-    });
-
-
-}
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (1 + max - min) + min);
-
-}
-
-function randomCharacter (){
-    var employee = {};
-    $.ajax({
-        type:'GET',
-        url:'/characters',
-        success: function(response){
-            console.log(response);
-            results = response.data.results;
-            if(results.length > 0) {
-                randomNum = randomNumber(0,results.length -1);
-                marvelApiImage = results[randomNum].thumbnail.path + "/standard_xlarge.jpg";
-                marvelCharId = results[randomNum].id;
-                marvelName = results[randomNum].name;
-                if(results[randomNum].description.length > 0) {
-                    marvelApiDescription = results[randomNum].description;
-                } else {
-                    marvelApiDescription = "No Back Story Available ";
-                }
-
-            } else {
-                marvelApiImage = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
-                marvelApiDescription = "No Back Story Available ";
-            }
-            employee.employeeTitle = marvelName;
-            employee.image = marvelApiImage;
-            employee.description = marvelApiDescription;
-            employee.employeeId = marvelCharId;
-            marvelArray.push(response);
-            AppendDom(employee);
-        }
-    });
-}
+//////////////////
+// append api callback info to the dom.
+/////////////////
 
 function AppendDom(employee) {
     primaryKey++;
@@ -173,44 +127,8 @@ function AppendDom(employee) {
     $el.append('<button class="btn btn-primary moreinfo"> More Info </button>');
 }
 
-//function randomComic (){
-//    var offset = randomNumber(1,36508);
-//    var employee = {};
-//    $.ajax({
-//        type:'GET',
-//        url: "http://gateway.marvel.com:80/v1/public/comics?limit=1&offset="+offset+"&",
-//        data:
-//        {"apikey":apiKey,
-//            "ts": apiTs,
-//            "hash": apiHash},
-//        headers: {   Accept:'*/*' },
-//        success: function(response){
-//            console.log(response);
-//            results = response.data.results;
-//            if(results.length > 0) {
-//                randomNum = randomNumber(0,results.length -1);
-//                marvelApiImage = results[randomNum].thumbnail.path + "/standard_xlarge.jpg";
-//                marvelCharId = results[randomNum].id;
-//                marvelName = results[randomNum].title;
-//                if(results[randomNum].description !== null) {
-//                    marvelApiDescription = results[randomNum].description;
-//                } else {
-//                    marvelApiDescription = "No description Available ";
-//                }
-//                console.log(marvelCharId);
-//            } else {
-//                marvelApiImage = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
-//                marvelApiDescription = "No description Available ";
-//            }
-//            employee.employeeTitle = marvelName;
-//            employee.image = marvelApiImage;
-//            employee.description = marvelApiDescription;
-//            employee.employeeId = marvelCharId;
-//            marvelArray.push(response);
-//            AppendDomComic(employee);
-//        }
-//    });
-//}
+
+
 function AppendDomComic(employee) {
     primaryKey++;
     employee.key = primaryKey;
@@ -225,6 +143,10 @@ function AppendDomComic(employee) {
     $el.append('<button class="btn btn-danger delete"> Remove Employee </button>');
     //$el.append('<button class="btn btn-primary moreinfo"> More Info </button>');
 }
+
+//////////////
+// pick a random comic,character or series ajax calls.
+////////////
 function randomSeries (){
     var employee = {};
     $.ajax({
@@ -294,3 +216,35 @@ function randomComic (){
     });
 }
 
+function randomCharacter (){
+    var employee = {};
+    $.ajax({
+        type:'GET',
+        url:'/characters',
+        success: function(response){
+            console.log(response);
+            results = response.data.results;
+            if(results.length > 0) {
+                randomNum = randomNumber(0,results.length -1);
+                marvelApiImage = results[randomNum].thumbnail.path + "/standard_xlarge.jpg";
+                marvelCharId = results[randomNum].id;
+                marvelName = results[randomNum].name;
+                if(results[randomNum].description.length > 0) {
+                    marvelApiDescription = results[randomNum].description;
+                } else {
+                    marvelApiDescription = "No Back Story Available ";
+                }
+
+            } else {
+                marvelApiImage = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg";
+                marvelApiDescription = "No Back Story Available ";
+            }
+            employee.employeeTitle = marvelName;
+            employee.image = marvelApiImage;
+            employee.description = marvelApiDescription;
+            employee.employeeId = marvelCharId;
+            marvelArray.push(response);
+            AppendDom(employee);
+        }
+    });
+}
